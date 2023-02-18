6d94a205defb7811a931ce0c8e74c510d4c44a30
Isaac Liljeros <isaak.liljeros@gmail.com>
2023-02-17 20:24:22 -0500

    Initial UI Development

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Fa from 'svelte-fa/src/fa.svelte'
+  import type {IconDefinition} from '@fortawesome/fontawesome-common-types'
+
+
+  /** Import the refs/icons for the pages */
+  export let href: string 
+  export let icon: IconDefinition
+  export let scale: number = 1.3
+  
+  // export let icon: any | undefined
+
+
+</script>
+
+<div class="bg-gray-800 hover:bg-gray-600 mb-1 h-12 w-12 rounded-lg relative 
+             transition-all duration-200 hover:shadow-lg">
+  <Fa class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" {icon} {scale}/>
+  <a class="absolute inset-0" {href}> </a>
+</div>
--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -3,44 +3,32 @@ import { execSync } from 'child_process'
 import { json } from "@sveltejs/kit"
 
 export const GET = (({ url }) => {
-
   // /dev/sdb /mnt/data btrfs rw,relatime,degraded,space_cache=v2,subvolid=5,subvol=/ 0 0
-  const result = execSync("cat /proc/mounts | grep btrfs").toString()
-  const resultArr = result.trim().split(' ')
 
-  const mountPoint = resultArr[1]
+//   const result = execSync("cat /proc/mounts | grep btrfs").toString()
+//   const resultArr = result.trim().split(' ')
 
-  console.log(`Info from ${mountPoint}\n`)
+//   const mountPoint = resultArr[1]
 
-  const ignore = ['', 'Free', 'estimated', ':', 'Data', 'ratio:', 'Used:', 'Device', 'size:']
-  const volumeInfo = execSync(`btrfs subvolume show ${mountPoint}`).toString()
-  const freeSpace = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Free (estimated)"`)
-          .toString()
-          .split(/[ \n\t\(\)]/)
-          .map(s => s.trim())
-          .filter(s => !ignore.includes(s))
-          .at(0)
-  const ratio = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Data ratio"`)
-          .toString()
-          .split(/[ \n\t\(\)]/)
-          .map(s => s.trim())
-          .filter(s => !ignore.includes(s)).at(0)
-  const usage = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Used:"`)
-          .toString().split(/[ \n\t\(\)]/).map(s => s.trim()).filter(s => !ignore.includes(s)).at(0)
-  const size = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Device size:"`)
-          .toString().split(/[ \n\t\(\)]/).map(s => s.trim()).filter(s => !ignore.includes(s)).at(0)
+//   console.log(`Info from ${mountPoint}\n`)
+
+//   const ignore = ['', 'Free', 'estimated', ':', 'Data', 'ratio:', 'Used:', 'Device', 'size:']
+//   const volumeInfo = execSync(`btrfs subvolume show ${mountPoint}`).toString()
+//   const freeSpace = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Free (estimated)"`).toString().split(/[ \n\t\(\)]/).map(s => s.trim()).filter(s => !ignore.includes(s)).at(0)
+//   const ratio = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Data ratio"`).toString().split(/[ \n\t\(\)]/).map(s => s.trim()).filter(s => !ignore.includes(s)).at(0)
+//   const usage = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Used:"`).toString().split(/[ \n\t\(\)]/).map(s => s.trim()).filter(s => !ignore.includes(s)).at(0)
+//   const size = execSync(`sudo btrfs filesystem usage -b ${mountPoint} | grep "Device size:"`).toString().split(/[ \n\t\(\)]/).map(s => s.trim()).filter(s => !ignore.includes(s)).at(0)
 
   //console.log(volumeInfo)
-  console.log('Freespace: ' + freeSpace)
-  console.log('Usage:     ' + usage)
-  console.log(`Size:      ${size}`)
-  console.log('Ratio:     ' + ratio + '\n')
+//   console.log('Freespace: ' + freeSpace)
+//   console.log('Usage:     ' + usage)
+//   console.log(`Size:      ${size}`)
+//   console.log('Ratio:     ' + ratio + '\n')
 
-  console.log(`Percent:   ${ ( Number(usage) / Number(size) * 100 ).toFixed(2)}%`)
+//   console.log(`Percent:   ${ ( Number(usage) / Number(size) * 100 ).toFixed(2)}%`)
 
-  const buf: string = execSync('pwd').toString();
-
-  const lsResult: string = buf.split('\n').join(" | ")
   
-  return json(buf.split('\n').join(" | "));
+  const buf: string = execSync('pwd').toString();
+  
+  return json(buf);
 }) satisfies RequestHandler;
