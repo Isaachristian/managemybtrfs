47cc2b7e202e1dbe2a9c7f7f2c9e4979529cee67
Isaachristian <isaac.liljeros@knights.ucf.edu>
2023-02-18 12:41:21 -0500

    Pages are created and made progress on general stats backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
+	let serverResult: string = 'Click the above button...'
 
-  let serverResult: string = "Click the above button..."
+	async function runCommandOnBackend() {
+		const response = await fetch('/', {
+			method: 'GET'
+		})
 
-  async function runCommandOnBackend() {
-    const response = await fetch('/', {
-      method: 'GET',
-    });
-
-    serverResult = await response.json();
-  }
-
+		serverResult = await response.json()
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
-<button
-  on:click={() => runCommandOnBackend()}>
-  LS the server directory
+<button on:click={() => runCommandOnBackend()}>
+	LS the server directory
 </button>
 
 <div>
-  {serverResult}
+	{serverResult}
 </div>
